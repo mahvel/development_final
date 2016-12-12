117f93847b3b9393dea19afab8f4e4980f45d770
mahvel <a1200463@myy.haaga-helia.fi>
2016-12-12 12:46:36 +0200

    With apache userdir configs

--- init.pp
+++ init.pp
@@ -1,13 +1,49 @@
-class development ($userName){
+class development {
 	
 	Package{ensure => "installed",}
 	$devpackages = ["mysql-server", "mysql-client", "netbeans", "mysql-workbench", "git", "gedit", "ssh"]
-	
-	package{$devpackages:
-		ensure => "installed",
-	}
 
 	file{"/home/$userName/workspace":
 		ensure => "directory",
+	}
+
+	file{"/home/$userName/public_html":
+		ensure => "directory",
+	}
+
+	file{"/etc/apache2/mods-enabled/userdir.conf":
+		ensure => "link",
+		target => "../mods-available/userdir.conf",
+		require => Package["apache2"],
+		notify => Service["apache2"],
+	}
+
+	file{"/etc/apache2/mods-enabled/userdir.load": 
+                ensure => "link",
+                target => "../mods-available/userdir.load",
+                require => Package["apache2"],
+                notify => Service["apache2"],
+        }
+
+	file{"/var/www/html":
+		ensure => "present",
+		content => template("development/index.html"),
+		require => Package["apache2"],
+		notify => Service["apache2"],
+	}
+
+	file{"/home/$userName/public_html/index.html":
+		ensure => "present",
+		content => template("development/index.html"),
+	}
+
+	service{"apache2":
+		ensure => "running",
+		enable => "true",
+	}
+
+	service{"ssh":
+		ensure => "running",
+		enable => "true",
 	}
 }
